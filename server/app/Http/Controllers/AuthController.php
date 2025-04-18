4ab6277f7d5906106384f36cc1d299f24e7ea32c
yui_dayal <murayamayuui@Yuis-MacBook-Pro.local>
2025-04-18 12:24:51 +0900

    created check session

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -13,14 +13,34 @@ class AuthController extends Controller {
         $this->authManager = new AuthManager();
     }
 
+    public function checkSession(Request $request)
+    {
+        if ($request->session()->has("user")) {
+            return response()->json([
+                'loggedIn' => true,
+                'user' => $request->session()->get('user')
+            ]);
+        }
+
+        return response()->json(['loggedIn' => false]);
+    }
+
     public function login(Request $request) 
     {
         $user = $this->authManager->login($request);
+        $request->session()->put('user', $user);
+
         return response()->json($user);
     }
 
     public function register(Request $request)
     {
         $user = $this->authManager->register($request);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->session()->forget('user');
+        return response()->json(['message'=> 'Logged out successfully']);
     }
 }
